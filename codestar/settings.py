f3bb476b7ebc3e01cc78596f54c33ecc2fac517e
ShanySB <shanyb.sb@gmail.com>
2026-07-25 20:49:26 +0100

    created about app and added 1st post

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = 'django-insecure-xhbz-+t)^-j$$m5zny0&6+o@*12fq^6*!&yn-jg(rurkm-dcqi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["127.0.0.1", "localhost", ".herokuapp.com"]
 
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_summernote',
     'blog',
+    'about',
 ]
 
 MIDDLEWARE = [
