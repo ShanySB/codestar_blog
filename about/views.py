4bea4e0b2c7e5213eefa20be18123530a6533c9e
ShanySB <shanyb.sb@gmail.com>
2026-08-01 21:26:34 +0100

    Created collaboration form and wire it up to recieve request to the database

--- about/views.py
+++ about/views.py
@@ -1,16 +1,29 @@
 from django.shortcuts import render
+from django.contrib import messages
 from .models import About
+from .forms import CollaborateForm
 
 
 # Create your views here.
 def about(request):
+
+    if request.method == "POST":
+        collaborate_form = CollaborateForm(data=request.POST)
+        if collaborate_form.is_valid():
+            collaborate_form.save()
+            messages.add_message(request, messages.SUCCESS, "Collaboration " \
+            "request recieved! I endeavor to respond within 2 working days.")
     """
     Renders the About page
     """
     about = About.objects.all().order_by('-updated_on').first()
+    collaborate_form = CollaborateForm()
 
     return render(
         request,
         "about/about.html",
-        {"about": about},
+        {
+            "about": about,
+            "collaborate_form": collaborate_form,
+        },
     )
